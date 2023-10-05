230f0e30c55729a97a3b392a65d51b2999a9d40e
Nikhil <nikhil@tikao.in>
2023-10-05 16:13:56 +0530

    Finished 9 parts of svelte tutorial.

--- hello-world/src/App.svelte
+++ hello-world/src/App.svelte
@@ -1,3 +1,4 @@
+<!-- SCRIPT SECTION IN SVELTE FILE FOR LOGIC -->
 <script>
   const name = "Freqs";
   const channel = "<b>freq808</b>";
@@ -8,23 +9,49 @@
   const status = "success";
   const isPromoted = true;
   const promoted = true;
+  const num = "Hi";
 </script>
 
+<!-- MARKUP/HTML SECTION IN SVELTE FILE FOR HTML TO RENDER -->
 <main>
+  <!-- BASIC TEXT BINDING -->
   <h1>Hello {name}</h1>
+
+  <!-- BASIC HTML BINDING -->
   <h2>{@html channel}</h2>
   <h2>{@html hack}</h2>
+
+  <!-- BASIC ATTRIBUTE BINDING -->
   <h2 id={headingId}>This is a heading with id</h2>
+  <!-- SHORTHAND -->
   <h2 {id}>This is a heading with id shorthand</h2>
+  <!-- BOOLEAN -->
   <button disabled={!disabled}>Not Disabled button</button>
   <button {disabled}>Disabled button</button>
+
+  <!-- BASIC CLASS BINDING -->
+  <!-- STATIC -->
   <h2 class="underline">Underlined text</h2>
+  <!-- DYNAMIC -->
   <h3 class={status}>Status</h3>
+  <!-- CONDITIONAL -->
   <h3 class={isPromoted ? "promoted" : ""}>Italic Text</h3>
   <h3 class:promoted={isPromoted}>Italic Text with svelte class directive</h3>
   <h3 class:promoted>Italic Text with svelte class directive shorthand</h3>
+
+  <!-- BASIC CONDITIONAL RENDERING -->
+  {#if num === 0}
+    <h2>The number is zero.</h2>
+  {:else if num > 0}
+    <h2>The number is positive.</h2>
+  {:else if num < 0}
+    <h2>The number is negative.</h2>
+  {:else}
+    <h2>Not a number.</h2>
+  {/if}
 </main>
 
+<!-- STYLE SECTION IN SVELTE FILE FOR CSS/STYLING -->
 <style>
   main {
     text-align: center;
